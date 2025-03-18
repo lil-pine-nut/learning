07fa54e0071c662b6ce4f04b0afacadac3adc5c5
lil-pine-nut <54873398+lil-pine-nut@users.noreply.github.com>
2025-03-18 10:19:00 +0800

    Update Log.cpp

--- log/log_h-spdlog/Log.cpp
+++ log/log_h-spdlog/Log.cpp
@@ -18,21 +18,29 @@ bool InitLogger(string logdir, string level, string pattern)
         /* 通过multi-sink的方式创建复合logger，实现方式为：先分别创建daily sink和控制台sink，
         并将两者放入sink 向量中，组成一个复合logger */
 
-		/* daily sink */
-		auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(logdir+"/log.log", 0, 0);
-		/* 控制台sink */
-		auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	/* daily sink */
+	// base_filename: 指定日志文件的基础名称。实际的日志文件名将包括日期信息，例如 "base_filename_2024-01-24.txt"。
+	// rotation_hour: 指定每天进行日志滚动的小时数（24小时制）。默认值为 0。
+	// rotation_minute: 指定每天进行日志滚动的分钟数。默认值为 0。
+	// truncate: 如果设置为 true，则在滚动日志时截断已存在的日志文件而不是追加。默认值为 false。重启软件输出日志时，true会清空改天日志，false是追加
+	// max_files: 指定保留日志文件的最大数量。超过这个数量的日志文件将被删除。默认值为 std::numeric_limits<int>::max()，即不限制日志文件数量。
+	// auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(logdir+"/log.log", 0, 0, false, 30);
+	auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(logdir+"/log.log", 0, 0);
+	/* 控制台sink */
+	auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
-		daily_sink->set_pattern(pattern);
-		// daily_sink->set_pattern("[%Y-%m-%d %T][%l]%v");
+	daily_sink->set_pattern(pattern);
+	// daily_sink->set_pattern("[%Y-%m-%d %T][%l]%v");
 
-		console_sink->set_pattern(pattern);
-		// console_sink->set_pattern("%+"); // spdlog's default format 等级在控制台会输出颜色
+	console_sink->set_pattern(pattern);
+	// %^%l%$：日志级别，%^ 和 %$ 之间的文本会被着色
+    	// console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e][%^%l%$][%s %!:%#] %v");
+    	// console_sink->set_pattern("%+"); //默认格式的日志等级在控制台会输出颜色
 
-        /* Sink组合 */
-		std::vector<spdlog::sink_ptr> sinks;
-        sinks.push_back(daily_sink);
-		sinks.push_back(console_sink);
+	/* Sink组合 */
+	std::vector<spdlog::sink_ptr> sinks;
+	sinks.push_back(daily_sink);
+	sinks.push_back(console_sink);
 
         g_logger = std::make_shared<spdlog::logger>("multi-sink", begin(sinks), end(sinks));
         spdlog::register_logger( g_logger );
